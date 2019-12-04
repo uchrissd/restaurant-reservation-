053cae08a54d236de5bb95dcb17cebd5e32535b5
uchrissd <uchrissd@protonmail.com>
2019-12-03 19:32:59 -0800

    started building server js

--- server.js
+++ server.js
@@ -12,36 +12,23 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
+//Reservation array
+var reservationsArray = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    customerName: "",
+    customerEmail: "",
+    customerId: "",
+    phoneNumber: ""
   }
 ];
 
-// Routes
-// =============================================================
+//Tables array
+var tablesArray = [];
 
-// Basic route that sends the user first to the AJAX Page
+//Waitlist array
+var waitlistArray = [];
+
+// Routes
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
